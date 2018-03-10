dd783192f12f4ba4508d793c1ddd63b62cc03e82
github username <github email address>
2018-03-10 15:30:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+//var allows the varaible to be assigned to a diff value
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+};
+
+//declaring without const,var, or let gives it global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+};
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+};
+
+//declaring with const throws a TypeError
+const leastFavoriteCustomer = 'me'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'type error?'
+}
